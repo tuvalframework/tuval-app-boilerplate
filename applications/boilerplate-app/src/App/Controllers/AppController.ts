d3e695f0a8381b700d2fe8430f090a5346f9b65b
selimtan <stan@bpmgenesis.com>
2022-07-01 10:11:30 +0300

    .

--- applications/boilerplate-app/src/App/Controllers/AppController.ts
+++ applications/boilerplate-app/src/App/Controllers/AppController.ts
@@ -1,28 +1,4 @@
-import {
-    CornerRadiusTypes,
-    cTopLeading,
-    ForEach,
-    HStack,
-    Icon,
-    IconLibrary,
-    IconType,
-    ScrollView,
-    Text,
-    UIController,
-    UIScene,
-    VStack,
-    cVertical,
-    PositionTypes,
-    Color,
-    Cache,
-    UIButton,
-    State,
-    cTop
-} from '@tuval/forms';
-import { TForm, cLeading } from '@tuval/forms';
-
-
-
+import { cTop, HStack, State, Text, TForm, UIController, UIScene, VStack } from '@tuval/forms';
 
 export class AppController extends UIController {
 
@@ -30,7 +6,7 @@ export class AppController extends UIController {
     private menu_text: string;
 
     protected InitController() {
-       this.menu_text  = 'About';
+        this.menu_text = 'About';
     }
 
     public OnBindModel(form: TForm) {
@@ -38,17 +14,8 @@ export class AppController extends UIController {
     }
     public LoadView() {
         return UIScene(
-            VStack({alignment:cTop})(
-                HStack({spacing:10})(
-                    Text(this.menu_text).cornerRadius(10).background({hover:'#EEE'}).padding(5)
-                    .onClick(()=> this.menu_text =  'New About'),
-                    Text('Test').cornerRadius(10).background({hover:'#EEE'}).padding(5),
-                    Text('For').cornerRadius(10).background({hover:'#EEE'}).padding(5)
-                )
-                .height()
-                .cursor('pointer')
-                .background('white')
-                .shadow('0 1px 2px hsla(0,0%,0%,0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05), 0 2px 8px hsla(0, 0%, 0%, 0.05)')
+            VStack({ alignment: cTop })(
+                Text('Hello Tuval').fontSize(20)
             )
 
         )
